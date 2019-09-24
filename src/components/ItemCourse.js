56b04b74ab15a14a404bc9991a8a5eb483d034d5
Ron Broek <r.t.broek@hotmail.com>
2019-09-24 14:23:14 +0200

    Added create, delete and updates for courses

--- src/components/ItemCourse.js
+++ src/components/ItemCourse.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import TitleItem from './TitleItem';
 import Dot from './Dot';
+
+import EditModeContext from '../contexts/editMode';
 
 const styles = {
   totalItem: {
@@ -15,8 +19,9 @@ const styles = {
   }
 };
 
-const ItemCourse = ({ classes, course, setUpdates }) => {
+const ItemCourse = ({ classes, course, setUpdates, deleteCourse }) => {
   const [state, setState] = useState(course);
+  const [editMode] = useContext(EditModeContext);
 
   const update = (e, content, id) => {
     const newState = {
@@ -31,15 +36,29 @@ const ItemCourse = ({ classes, course, setUpdates }) => {
     <article className={classes.totalItem}>
       <Grid container alignItems="stretch">
         <Dot />
-        <Grid item xs={10} className={classes.textPart}>
-          <TitleItem
-            title={state.name}
-            subtitle={state.instituteName}
-            gutterBottom={false}
-            titleName="name"
-            subtitleName="instituteName"
-            setUpdates={update}
-          />
+        <Grid container item xs={10} className={classes.textPart}>
+          <Grid item xs={11}>
+            <TitleItem
+              title={state.name}
+              subtitle={state.instituteName}
+              gutterBottom={false}
+              titleName="name"
+              subtitleName="instituteName"
+              setUpdates={update}
+            />
+          </Grid>
+
+          {editMode ? (
+            <Grid item xs={1}>
+              <IconButton
+                aria-label="delete"
+                className={classes.margin}
+                onClick={() => deleteCourse(course._id)}
+              >
+                <DeleteIcon color="error" />
+              </IconButton>
+            </Grid>
+          ) : null}
         </Grid>
       </Grid>
     </article>
--- src/components/SectionCourses.js
+++ src/components/SectionCourses.js
@@ -1,25 +1,64 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import Book from '@material-ui/icons/Book';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 import StyledPaper from './StyledPaper';
 import TitleSection from './TitleSection';
 import ItemCourse from './ItemCourse';
 
+import useRequest from '../hooks/useRequest';
+import EditModeContext from '../contexts/editMode';
 import CvContext from '../contexts/cv';
+import AuthContext from '../contexts/auth';
 
 export const sortByName = (a, b) => (a.name > b.name ? 1 : -1);
 
 const SectionCourses = ({ courses }) => {
   const [state, setState] = useState(courses);
-  const { requestUpdatesCvModel } = useContext(CvContext);
+  const { requestUpdatesCvModel, currentCv } = useContext(CvContext);
+  const [editMode] = useContext(EditModeContext);
+  const [auth] = useContext(AuthContext);
+  const [response, makeRequest] = useRequest(process.env.NODE_HOST);
 
-  const updateItem = update => {
-    const newListOfCourses = state.list.filter(course => course._id !== update._id).concat(update);
+  const createCourse = async () => {
+    const newCourse = await makeRequest(`/cvs/${currentCv}/courses`, 'POST', {
+      headers: {
+        authorization: `Bearer ${auth.token}`
+      }
+    });
+
+    const newListOfCourses = state.list.concat([newCourse]);
     const newState = { ...state, list: newListOfCourses };
 
     setState(newState);
-    requestUpdatesCvModel({ courses: newState });
+  };
+
+  const deleteCourse = async id => {
+    const newListOfStudies = state.list.filter(course => course._id !== id);
+    const newState = { ...state, list: newListOfStudies };
+
+    await makeRequest(`/cvs/${currentCv}/courses/${id}`, 'DELETE', {
+      headers: {
+        authorization: `Bearer ${auth.token}`
+      }
+    });
+    setState(newState);
+  };
+
+  const updateStudy = async update => {
+    const newListOfStudies = state.list.filter(study => study._id !== update._id).concat(update);
+    const newState = { ...state, list: newListOfStudies };
+
+    await makeRequest(`/cvs/${currentCv}/courses/${update._id}`, 'PATCH', {
+      headers: {
+        authorization: `Bearer ${auth.token}`
+      },
+      data: update
+    });
+
+    setState(newState);
   };
 
   const updateTitle = (e, content, id) => {
@@ -38,7 +77,27 @@ const SectionCourses = ({ courses }) => {
         {state &&
           state.list
             .sort(sortByName)
-            .map(course => <ItemCourse course={course} key={course._id} setUpdates={updateItem} />)}
+            .map(course => (
+              <ItemCourse
+                course={course}
+                key={course._id}
+                setUpdates={updateStudy}
+                deleteCourse={deleteCourse}
+              />
+            ))}
+
+        {editMode ? (
+          <Grid justify="center" container>
+            <Button
+              variant="contained"
+              color="primary"
+              aria-label="Add new education"
+              onClick={createCourse}
+            >
+              Add course
+            </Button>
+          </Grid>
+        ) : null}
       </StyledPaper>
     </section>
   );
